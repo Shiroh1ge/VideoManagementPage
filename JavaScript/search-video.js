424f29ecb7d2c8327edf0eda8294d65216029f47
Borislav Doichinov <hakera_bg@msn.com>
2015-09-20 02:24:11 +0300

    Tweaked the slider

--- JavaScript/search-video.js
+++ JavaScript/search-video.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
     $(function() {
         $( "#slider-range" ).slider({
             range: true,
-            min: 0,
-            max: 500,
-            values: [ 75, 300 ],
+            min: 2000,
+            max: 8500,
+            values: [ 2000, 8000 ],
+            step:100,
             slide: function( event, ui ) {
-                $( "#dl-sv-input-mmr" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+                $( "#dl-sv-input-mmr" ).val(  ui.values[ 0 ] + " - " + ui.values[ 1 ] );
             }
         });
-        $( "#dl-sv-input-mmr" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+        $( "#dl-sv-input-mmr" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+            " - " + $( "#slider-range" ).slider( "values", 1 ) );
     });
 });
 
